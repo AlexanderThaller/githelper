34ff3e6a6d939df347ac91ab6405cc3aabca15c4
Alexander Thaller <alexander.thaller@trivago.com>
2016-12-28 17:46:36 +0100

    Removed dependency on git2 and switched to a cli based solution (cant build git2 for some reason...).

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,86 @@
-extern crate git2;
-
-use git2::{Repository, Error, ObjectType};
+use std::io::Result;
 use std::path::Path;
+use std::process::Command;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+pub fn get_current_commitid_for_repo(folder: &Path) -> Result<String> {
+    let output = Command::new("git").arg("rev-parse")
+        .arg("HEAD")
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
 }
 
-pub fn get_current_commitid_for_repo(folder: &Path) -> Result<String, Error> {
-    let repo = try!(Repository::open(&folder));
-    let head = try!(repo.head());
-    let head_name = try!(head.resolve());
-    let commit = try!(head_name.peel(ObjectType::Commit));
-    let id = commit.id();
+pub fn clone(folder: &Path, remote: &str) -> Result<String> {
+    let output = Command::new("git").arg("clone")
+        .arg(remote)
+        .arg(folder)
+        .output()?;
 
-    Ok(format!("{}", id))
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+#[test]
+fn test_git_clone() {
+    use std::path::PathBuf;
+    let out = assert!(clone(PathBuf::from("/tmp/git_test_clone").as_path(), ".").is_ok());
+    std::fs::remove_dir_all("/tmp/git_test_clone").unwrap();
+
+    out
+}
+
+pub fn add(folder: &Path, file: &Path) -> Result<String> {
+    let output = Command::new("git").arg("add")
+        .arg(file)
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+pub fn commit(folder: &Path, message: &str) -> Result<String> {
+    let output = Command::new("git").arg("commit")
+        .arg("-m")
+        .arg(message)
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+pub fn status(folder: &Path) -> Result<String> {
+    let output = Command::new("git").arg("status")
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+pub fn init(folder: &Path) -> Result<String> {
+    let output = Command::new("git").arg("init")
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+pub fn pull(folder: &Path) -> Result<String> {
+    let output = Command::new("git").arg("pull")
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+pub fn push(folder: &Path) -> Result<String> {
+    let output = Command::new("git").arg("push")
+        .current_dir(folder)
+        .output()?;
+
+    Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
+}
+
+pub fn sync(folder: &Path) -> Result<String> {
+    pull(&folder)?;
+    push(&folder)
 }
