8d1f5707f7db2bb330464d60fa56e549b0fae152
Alexander Thaller <alexander.thaller@trivago.com>
2019-11-18 18:05:44 +0100

    Moved to rust edition 2018

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-#[macro_use]
-extern crate log;
-
-use std::io::Result;
-use std::io::Error;
-use std::io::ErrorKind;
-use std::path::Path;
-use std::process::Command;
+use log::{
+    debug,
+    trace,
+};
+use std::{
+    io::{
+        Error,
+        ErrorKind,
+        Result,
+    },
+    path::Path,
+    process::Command,
+};
 
 pub fn get_current_commitid_for_repo(folder: &Path) -> Result<String> {
-    let output = Command::new("git").arg("rev-parse")
+    let output = Command::new("git")
+        .arg("rev-parse")
         .arg("HEAD")
         .current_dir(folder)
         .output()?;
@@ -18,13 +24,16 @@ pub fn get_current_commitid_for_repo(folder: &Path) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
 pub fn clone(folder: &Path, remote: &str) -> Result<String> {
-    let output = Command::new("git").arg("clone")
+    let output = Command::new("git")
+        .arg("clone")
         .arg(remote)
         .arg(folder)
         .output()?;
@@ -34,8 +43,10 @@ pub fn clone(folder: &Path, remote: &str) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
@@ -49,7 +60,8 @@ fn test_git_clone() {
 }
 
 pub fn add(folder: &Path, file: &Path) -> Result<String> {
-    let output = Command::new("git").arg("add")
+    let output = Command::new("git")
+        .arg("add")
         .arg(file)
         .current_dir(folder)
         .output()?;
@@ -59,13 +71,16 @@ pub fn add(folder: &Path, file: &Path) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
 pub fn commit(folder: &Path, message: &str) -> Result<String> {
-    let output = Command::new("git").arg("commit")
+    let output = Command::new("git")
+        .arg("commit")
         .arg("-m")
         .arg(message)
         .current_dir(folder)
@@ -76,13 +91,16 @@ pub fn commit(folder: &Path, message: &str) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
 pub fn status(folder: &Path) -> Result<String> {
-    let output = Command::new("git").arg("status")
+    let output = Command::new("git")
+        .arg("status")
         .current_dir(folder)
         .output()?;
 
@@ -91,13 +109,16 @@ pub fn status(folder: &Path) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
 pub fn init(folder: &Path) -> Result<String> {
-    let output = Command::new("git").arg("init")
+    let output = Command::new("git")
+        .arg("init")
         .current_dir(folder)
         .output()?;
 
@@ -106,15 +127,18 @@ pub fn init(folder: &Path) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
 pub fn pull(folder: &Path) -> Result<String> {
     debug!("pulling git repo in folder {:#?}", folder);
 
-    let output = Command::new("git").arg("pull")
+    let output = Command::new("git")
+        .arg("pull")
         .current_dir(folder)
         .output()?;
 
@@ -123,15 +147,18 @@ pub fn pull(folder: &Path) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
 pub fn push(folder: &Path) -> Result<String> {
     debug!("pushing git repo in folder {:#?}", folder);
 
-    let output = Command::new("git").arg("push")
+    let output = Command::new("git")
+        .arg("push")
         .current_dir(folder)
         .output()?;
 
@@ -140,8 +167,10 @@ pub fn push(folder: &Path) -> Result<String> {
     if output.status.success() {
         Ok(format!("{}", String::from_utf8_lossy(&output.stdout)))
     } else {
-        Err(Error::new(ErrorKind::Other,
-                       format!("{}", String::from_utf8_lossy(&output.stderr))))
+        Err(Error::new(
+            ErrorKind::Other,
+            format!("{}", String::from_utf8_lossy(&output.stderr)),
+        ))
     }
 }
 
